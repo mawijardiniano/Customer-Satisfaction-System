44a00bea4ce6d78a2974ec19a682538974b5ddb4
Mawi <jardinianomarklouisse@gmail.com>
2025-01-28 15:57:11 +0800

    create a login/signup route

--- Backend/middleware/authenticationToken.js
+++ Backend/middleware/authenticationToken.js
@@ -8,7 +8,6 @@ const authenticateToken = (req, res, next) => {
   if (!token) {
     return res.status(403).json({ message: 'Access denied, no token provided' });
   }
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
