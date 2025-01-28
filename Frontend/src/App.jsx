3b295666341e935b3b72af53c3fe7df16368cdc1
DarwinMelaya <116789003+DarwinMelaya@users.noreply.github.com>
2025-01-28 13:31:16 +0800

    Add a Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-function App() {
-  const [count, setCount] = useState(0);
+import Navbar from "./components/Navbar";
 
+function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/feedback" element={<Home />} />
+        <Route path="/contact" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
