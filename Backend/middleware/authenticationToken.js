49f3766204b1421bb98d2e23e9b82333f91b8726
Mawi <jardinianomarklouisse@gmail.com>
2025-01-29 14:19:21 +0800

    create a route or customers feedback

--- Backend/middleware/authenticationToken.js
+++ Backend/middleware/authenticationToken.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require("dotenv");
 
+
 const authenticateToken = (req, res, next) => {
 
   const token = req.headers['authorization']?.split(' ')[1];
@@ -8,6 +9,7 @@ const authenticateToken = (req, res, next) => {
   if (!token) {
     return res.status(403).json({ message: 'Access denied, no token provided' });
   }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
