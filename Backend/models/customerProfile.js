e0f829648e9811572c11741ddf11c436f7f7981b
Mawi <jardinianomarklouisse@gmail.com>
2025-01-29 13:55:30 +0800

    create a route for customers profile

---
+++ Backend/models/customerProfile.js
@@ -0,0 +1,87 @@
+const mongoose = require("mongoose");
+
+const customerProfileSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  institution: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  classification: {
+    type: String,
+    enum: [
+      "Student",
+      "Owner of a business",
+      "Employee of a business",
+      "Government employee",
+      "Professional",
+      "Overseas Filipino Worker",
+      "Not employed (retiree/displaced)",
+      "Others",
+    ],
+    required: true,
+  },
+  specificProfessional: {
+    type: String,
+    required: function () {
+      return this.classification === "Professional";
+    },
+    default: null,
+  },
+  otherClassification: {
+    type: String,
+    required: function () {
+      return this.classification === "Others";
+    },
+    default: null,
+  },
+
+  firstVisit: { type: Boolean, default: true },
+  gender: {
+    type: String,
+    enum: ["Male", "Female"],
+    required: true,
+  },
+  age: {
+    type: String,
+    enum: [
+      "15 & below",
+      "16-20",
+      "21-30",
+      "31-40",
+      "41-50",
+      "51-59",
+      "60 & above",
+    ],
+    required: true,
+  },
+  withDisability: { type: Boolean, default: true },
+  educationLevel: {
+    type: String,
+    enum: ["Elementary", "High School", "College", "Masters/PhD.", "Others"],
+    required: true,
+  },
+  otherEducation: {
+    type: String,
+    required: function () {
+      return this.educationLevel === "Others";
+    },
+    default: null,
+  },
+  service: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Services",
+  }],
+});
+
+module.exports = mongoose.model("customerProfile", customerProfileSchema);
